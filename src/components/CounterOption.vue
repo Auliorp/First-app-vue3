2bcd3839c7a99158424ce379a361f1c67fd8fea2
Aulio Rovero <aulioroveroparedes@gmail.com>
2024-04-09 18:44:48 -0400

    realizando contador con el tipo option API

---
+++ src/components/CounterOption.vue
@@ -0,0 +1,37 @@
+<template>
+<h3>{{ counter }}</h3>
+<button @click="addCounter">Incrementar</button>
+<button @click="subtractCounter">Decrementar</button>
+<button @click="resetCounter">Reiniciar</button>
+</template>
+
+<script lang="ts">
+//Option API
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: 'CounterOption',
+    components: {},
+    data(){
+let counter = 0;
+    return {counter}
+},
+methods: {
+    addCounter(){
+        this.counter++;
+    },
+    subtractCounter(){
+        if (this.counter > 0){
+            this.counter--;
+        }
+        
+    },
+    resetCounter(){
+        this.counter = 0;
+    }
+
+},
+})
+</script>
+
+<style>
+</style>
