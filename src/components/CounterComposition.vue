03bf4a9c86e5d3d1625c60039df357bb8d2ec71c
Aulio Rovero <aulioroveroparedes@gmail.com>
2024-04-10 16:49:08 -0400

    Realizando contador con el tipo Composition API

---
+++ src/components/CounterComposition.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="container">
+        <h3 class="title">{{ counter }}</h3>
+        <button @click="addCounter" class="btn">Incrementar</button>
+        <button @click="subtractCounter">Decrementar</button>
+        <button @click="resetCounter">Reiniciar</button>
+    </div>
+
+</template>
+
+<script lang="ts">
+import {defineComponent,Ref, ref} from "vue"
+export default defineComponent({
+name: 'CounterComposition',
+setup() {
+    let counter:Ref = ref(0);
+    const addCounter = ():void => {
+       
+        counter.value++
+       
+    }
+    let subtractCounter = ():void => {
+        if(counter.value > 0){
+            counter.value--
+}
+        
+    }
+    let resetCounter = ():void => {
+        counter.value = 0
+    }
+    return {counter, addCounter, subtractCounter, resetCounter}
+}
+})
+</script>
+
+<style>
+.container{
+    display: flex;
+    flex-direction: column;
+    background-color: gray;
+    border-radius: 5px;
+}
+.title{
+    display: flex;
+    justify-content: center;
+}
+.btn{
+    border-radius: 5px;
+}
+.btn:hover{
+    cursor: pointer;
+}
+</style>
