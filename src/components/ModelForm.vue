436656c9e24436806a402c91d27d9f2ea62bf15e
Aulio Rovero <aulioroveroparedes@gmail.com>
2024-04-15 17:14:13 -0400

    Continuacion, trabajando de forma basica en inputs de formulario y directivas v-model

--- src/components/ModelForm.vue
+++ src/components/ModelForm.vue
@@ -1,20 +1,28 @@
 <template>
 <form action="">
     <input type="text" v-model="inputText">
-    <h3>{{ inputText }}</h3>
+    <input type="checkbox" v-model="agree"> Acepto los terminos y condiciones
+    <select name="" id="" v-model="country">
+        <option value="Spain"> Spain </option>
+        <option value="Uk"> UK </option>
+        <option value="France"> France </option>
+    </select>
 </form>
+<h3>{{ inputText }}</h3>
 </template>
 
 //Estructura Composition API en su version actualizada *Con el setup*
-<!-- <script lang="ts" setup>
-import { Ref, ref } from 'vue';
+<script lang="ts" setup>
+    import { Ref, ref } from 'vue';
 
-let inputText:Ref<string> = ref("");
+    let inputText:Ref<string> = ref("");
+    let agree:Ref<boolean> = ref(true);
+    let country = ref("Uk")
 
-</script> -->
+</script>
 
 //Estructura Composition API en su version actualizada *Sin el setup*
-<script lang="ts">
+<!-- <script lang="ts">
 
 import { Ref, ref, defineComponent } from 'vue';
 export default defineComponent({
@@ -23,9 +31,19 @@ export default defineComponent({
             return {inputText}
     }
 })
+</script> -->
 
-
-</script>
+//Estructura Option API en su version estandar *Sin el setup*
+<!-- <script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+    data(){
+            return {
+                inputText: "", 
+            }
+    }
+})
+</script> -->
 
 <style scoped>
 </style>
